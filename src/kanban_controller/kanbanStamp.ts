639746f1955c1ebae1351b431aef55978d156869
Alex <flatalex123@gmail.com>
2023-09-21 20:08:04 +1000

    processing of kanban complete

--- src/kanban_controller/kanbanStamp.ts
+++ src/kanban_controller/kanbanStamp.ts
@@ -14,10 +14,18 @@ export class KanbanStamp {
 	}
 
 	public stamp() {
+		const backlogTag = "#b";
+		const inProgressTag = "#w";
+		const compleatedTag = "#d";
+		const archiveTag = "#Archive";
+		const blockerTag = "#blocker";
+
+		console.log("Start");
 		interface Item {
 			text: string;
-			startDateTime?: string;
-			endDateTime?: string;
+			startDatetime?: Date;
+			endDatetime?: Date;
+
 			isChecked: boolean;
 		}
 
@@ -33,13 +41,13 @@ export class KanbanStamp {
 		}
 
 		const activeLeaf: WorkspaceLeaf | null = app.workspace.activeLeaf;
-
 		if (!activeLeaf || !activeLeaf.view) {
 			new Notice(
 				"Couldn't access the editor. Make sure an editor pane is active."
 			);
 			return;
 		}
+		console.log("Start");
 
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		const file: TFile = (activeLeaf.view as any).file;
@@ -51,24 +59,16 @@ export class KanbanStamp {
 				content !== lastModifiedContent &&
 				lines[2] === this.KANBAN_PLUGIN_TAG
 			) {
-				getKanban(lines);
-				// const ban = getKanban(lines);
-				// let currentTag = "";
-				// Object.keys(ban).forEach(function (key) {
-				// 	const value = ban[key];
-				// 	currentTag = getTag(key);
-				// 	console.log(value);
-				// 	value.e.forEach((element, index) => {
-				// 		//Add tags
-				// 		element = removeTags(element);
-				// 		element = `${element} ${currentTag}`;
-				// 		value.e[index] = element;
-				// 		console.log(element);
-				// 	});
-				// 	// if()
-				// });
-				// //let currentTag = null;
-				// console.log(ban);
+				let kanban: Kanban = getKanban(lines);
+				kanban = rolloverTasks(kanban);
+				kanban = archiveTasks(kanban);
+				kanban = addCompleatedTime(kanban);
+				console.log(kanban);
+
+				kanban = addStartingTime(kanban);
+
+				console.log(kanban);
+				console.log(content);
 			}
 
 			const updatedContent = lines.join("\n");
@@ -78,6 +78,95 @@ export class KanbanStamp {
 				new Notice("Tags have been propagated!");
 			}
 		});
+
+		function addCompleatedTime(kan: Kanban): Kanban {
+			Object.keys(kan.headings).forEach((key) => {
+				const items = kan.headings[key].items;
+				for (let i = 0; i < items.length; i++) {
+					const item = items[i];
+					const today = new Date();
+
+					if (kan.headings[key].tag !== compleatedTag) {
+						return;
+					}
+					if (!item.endDatetime) {
+						kan.headings[key].items[i].endDatetime = today;
+					}
+				}
+			});
+			return kan;
+		}
+
+		function addStartingTime(kan: Kanban): Kanban {
+			Object.keys(kan.headings).forEach((key) => {
+				const items = kan.headings[key].items;
+				for (let i = 0; i < items.length; i++) {
+					const item = items[i];
+					const today = new Date();
+
+					if (kan.headings[key].tag !== inProgressTag) {
+						return;
+					}
+					if (!item.startDatetime) {
+						kan.headings[key].items[i].startDatetime = today;
+					}
+				}
+			});
+			return kan;
+		}
+
+		function archiveTasks(kan: Kanban): Kanban {
+			Object.keys(kan.headings).forEach((key) => {
+				const items = kan.headings[key].items;
+				for (let i = 0; i < items.length; i++) {
+					const item = items[i];
+					const today = new Date();
+
+					if (
+						item.endDatetime &&
+						!isSameDay(item.endDatetime, today)
+					) {
+						kan.headings["Archive #Archive"].items.push(item);
+						kan.headings[key].items.splice(i, 1);
+					}
+				}
+			});
+			return kan;
+		}
+
+		function rolloverTasks(kan: Kanban): Kanban {
+			Object.keys(kan.headings).forEach((key) => {
+				const items = kan.headings[key].items;
+				for (let i = 0; i < items.length; i++) {
+					const item = items[i];
+					const today = new Date();
+
+					if (
+						item.endDatetime ||
+						kan.headings[key].tag === blockerTag
+					) {
+						continue;
+					}
+
+					if (
+						item.startDatetime &&
+						!isSameDay(item.startDatetime, today)
+					) {
+						delete item.startDatetime;
+						kan.headings["Backlog #b"].items.push(item); // Update the item directly in the items array
+						kan.headings[key].items.splice(i, 1);
+						console.log("dasd");
+					}
+				}
+			});
+			return kan;
+		}
+
+		function isSameDay(date1: Date, date2: Date) {
+			date1.setHours(0, 0, 0, 0);
+			date2.setHours(0, 0, 0, 0);
+			return date1.getTime() === date2.getTime();
+		}
 
 		function getKanban(lines: string[]) {
 			const kanban: Kanban = { headings: {}, completeLoc: "" };
@@ -101,7 +190,7 @@ export class KanbanStamp {
 					continue;
 				}
 
-				apendItem(line, kanban, currentTag, currentHeader);
+				appendItem(line, kanban, currentTag, currentHeader);
 
 				if (line.startsWith("**Complete**")) {
 					kanban.completeLoc = currentHeader;
@@ -111,7 +200,7 @@ export class KanbanStamp {
 			return kanban;
 		}
 
-		function apendItem(
+		function appendItem(
 			line: string,
 			kanban: Kanban,
 			currentTag: string,
@@ -119,27 +208,66 @@ export class KanbanStamp {
 		) {
 			if (isListItemOrTask(line)) {
 				const startDatetimeMatch = line.match(
-					/(🛫 \d{2}-\d{2}-\d{4} \d{1,2}:\d{2}[ap]m)/
+					/🛫 (\d{2}-\d{2}-\d{4}) (\d{1,2}:\d{2}[ap]m)/
 				);
 				const endDatetimeMatch = line.match(
-					/(✅ \d{2}-\d{2}-\d{4} \d{1,2}:\d{2}[ap]m)/
+					/✅ (\d{2}-\d{2}-\d{4}) (\d{1,2}:\d{2}[ap]m)/
 				);
-				const textMathc = line.match(
+				const textMatch = line.match(
 					/- \[[x ]\]\s*(.*?)\s*(?=(🛫 \d{2}-\d{2}-\d{4} \d{1,2}:\d{2}[ap]m|✅ \d{2}-\d{2}-\d{4} \d{1,2}:\d{2}[ap]m #\w+|$))/
 				);
+
 				const newLine: Item = {
-					text: textMathc ? textMathc[1] : "",
-					startDateTime: startDatetimeMatch
-						? startDatetimeMatch[1]
-						: "",
-					endDateTime: endDatetimeMatch ? endDatetimeMatch[1] : "",
+					text: textMatch ? textMatch[1] : "",
 					isChecked: isChecked(line),
 				};
-				if (currentTag === "#w" && !newLine.startDateTime) {
-					newLine.startDateTime = getCurrentDatetime();
+
+				if (startDatetimeMatch) {
+					newLine.startDatetime = combineDateAndTime(
+						parseDate(startDatetimeMatch[1]),
+						parseTime(startDatetimeMatch[2])
+					);
 				}
+
+				if (endDatetimeMatch) {
+					newLine.endDatetime = combineDateAndTime(
+						parseDate(endDatetimeMatch[1]),
+						parseTime(endDatetimeMatch[2])
+					);
+				}
+
+				if (currentTag === inProgressTag && !newLine.startDatetime) {
+					newLine.startDatetime = getCurrentDatetime();
+				}
+
 				kanban.headings[currentHeader].items.push(newLine);
 			}
+		}
+
+		function parseDate(dateStr: string): Date {
+			const [day, month, year] = dateStr.split("-").map(Number);
+			return new Date(year, month - 1, day);
+		}
+
+		function parseTime(timeStr: string): Date {
+			const [hourStr, minuteStr] = timeStr.slice(0, -2).split(":");
+			const isPM = timeStr.toLowerCase().endsWith("pm");
+			let hour = parseInt(hourStr, 10);
+			if (isPM && hour !== 12) {
+				hour += 12;
+			} else if (!isPM && hour === 12) {
+				hour = 0;
+			}
+			const minute = parseInt(minuteStr, 10);
+
+			const date = new Date();
+			date.setHours(hour, minute, 0, 0);
+			return date;
+		}
+
+		function combineDateAndTime(date: Date, time: Date): Date {
+			date.setHours(time.getHours(), time.getMinutes(), 0, 0);
+			return date;
 		}
 
 		// function removeTags(line: string) {
@@ -156,19 +284,21 @@ export class KanbanStamp {
 		function isChecked(line: string) {
 			return line.startsWith("- [x]");
 		}
-
-		const getCurrentDatetime = () => {
-			const currentDate = new Date();
-			const day = String(currentDate.getDate()).padStart(2, "0");
-			const month = String(currentDate.getMonth() + 1).padStart(2, "0");
-			const year = currentDate.getFullYear();
-			let hours = currentDate.getHours();
-			const ampm = hours >= 12 ? "pm" : "am";
-			hours = hours % 12;
-			hours = hours || 12;
-			const minutes = String(currentDate.getMinutes()).padStart(2, "0");
-			return `${day}-${month}-${year} ${hours}:${minutes}${ampm}`;
+		const getCurrentDatetime = (): Date => {
+			return new Date();
 		};
+		// const getCurrentDatetime = () => {
+		// 	const currentDate = new Date();
+		// 	const day = String(currentDate.getDate()).padStart(2, "0");
+		// 	const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+		// 	const year = currentDate.getFullYear();
+		// 	let hours = currentDate.getHours();
+		// 	const ampm = hours >= 12 ? "pm" : "am";
+		// 	hours = hours % 12;
+		// 	hours = hours || 12;
+		// 	const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+		// 	return `${day}-${month}-${year} ${hours}:${minutes}${ampm}`;
+		// };
 
 		function getTag(line: string) {
 			const tagMatches = line.match(/#[^\s#]+/g);
